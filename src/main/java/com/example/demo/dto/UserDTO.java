e19ddb82ca4cf528680ef18d3aa05c7c54f99012
OleksandrSamokysh <alekssanchez12@gmail.com>
2023-02-26 16:39:09 +0200

    added services

---
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String bio;
+
+}
